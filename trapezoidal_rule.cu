bc9a4517bf079d5a34d95f4e28e2871f3d20bb74
cmccla15 <carrickmcclain@gmail.com>
2018-04-25 15:53:42 -0400

    changes

--- trapezoidal_rule.cu
+++ trapezoidal_rule.cu
@@ -2,7 +2,7 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-float function( float input )
+float func( float input )
 {
     return 1/(1+input*input);
 }
@@ -10,17 +10,19 @@ float function( float input )
 float trapezoidal( float a, float b, float n )
 {
     float h = (b-a)/n;
-    float s = function(a) + function(b);
+    float s = func(a) + func(b);
     for( int i = 1; i < n; i++ )
     {
-        s += 2*function(a+i*h);
+        s += 2*func(a+i*h);
     }
     return (h/2)*s;
 }
 
-__global__ trapezoidal_kernel( float a, float b, float n, float (*f)(float, float, float) )
+__global__ void trapezoidal_kernel( float a, float b, float n, float (*f)(float), float* output )
 {
     int tid = threadIdx.x;
+
+
 }
 
 int main()
@@ -28,14 +30,16 @@ int main()
     float x0 = 0;
     float xn = 1;
     int n = 6;
+    float* kernel_output;
 
     printf("Serial: Value of integral is %6.4f\n", 
         trapezoidal(x0, xn, n));
     
-    //now the parallel part.
-
-
-
+    // now the parallel part.
+    dim3 dimGrid (32);
+    dim3 dimBlock (16, 16);
+    *kernel_output = 0;
+    trapezoidal_kernel<<<dimGrid, dimBlock>>>( x0, xn, n, func, kernel_output);
     
     return 0;
 }
