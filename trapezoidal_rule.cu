e9868395c501f8d9279797dd2ae0dab60b0d5342
cmccla15 <36656819+cmccla15@users.noreply.github.com>
2018-04-25 18:15:19 -0400

    Update trapezoidal_rule.cu

--- trapezoidal_rule.cu
+++ trapezoidal_rule.cu
@@ -2,6 +2,8 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
+// function
+// can replace with any other function
 float func( float input )
 {
     return 1/(1+input*input);
@@ -20,13 +22,20 @@ float trapezoidal( float a, float b, float n )
 
 __global__ void trapezoidal_kernel( float a, float b, float n, float (*f)(float), float* output )
 {
-    int tid = threadIdx.x;
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    float h = (b-a)/n;
+    float s = f(a) + f(b);
 
-
+    if( tid < n )   // might get messed up because of multiple blocks?
+    {
+        s+= 2*f(a+tid*h);
+    }
+    *output = (h/2)*s;
 }
 
 int main()
 {
+    cudaError_t err;
     float x0 = 0;
     float xn = 1;
     int n = 6;
@@ -36,10 +45,18 @@ int main()
         trapezoidal(x0, xn, n));
     
     // now the parallel part.
+    err = cudaMalloc(  );   // for kernel_output
+    gpu_err_chk(err);       // create this function above
+    err = cudaMemcpy( , , cudaMemcpyHostToDevice );   // for kernel_output
+    gpu_err_chk(err);
+    
     dim3 dimGrid (32);
     dim3 dimBlock (16, 16);
     *kernel_output = 0;
     trapezoidal_kernel<<<dimGrid, dimBlock>>>( x0, xn, n, func, kernel_output);
+    
+    err = cudaMemcpy( , , cudaMemcpyDeviceToHost );
+    gpu_err_chk(err);
     
     return 0;
 }
